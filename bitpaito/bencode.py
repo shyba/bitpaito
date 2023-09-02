f9d8543f488c03bce17db13b6c7b3dcb5387d1b3
Victor Shyba <victor.shyba@gmail.com>
2023-09-02 17:16:20 -0300

    bencode: decode strings

--- bitpaito/bencode.py
+++ bitpaito/bencode.py
@@ -3,3 +3,36 @@ def encode(what: str | bytes):
         what = what.encode()
     if isinstance(what, bytes):
         return str(len(what)).encode() + b':' + what
+
+
+def decode(what: bytes):
+    state = START
+    size = 0
+    ret = None
+    for c in what:
+        if state in (START, WANT_COLON):
+            if ord(b'0') <= c <= ord(b'9'):
+                state = WANT_COLON
+                size = size*10 + int(chr(c))
+            elif c == ord(b':'):
+                if state == WANT_COLON:
+                    state = WANT_ATOMS
+                    ret = ""
+                else:
+                    raise ValueError(f'Got a : in state {state}')
+            else:
+                raise ValueError(f'Unknown state {state} with token {chr(c)}')
+        elif state == WANT_ATOMS and size > 0:
+            ret += chr(c)
+            size -= 1
+            if size == 0:
+                return ret
+        else:
+            raise ValueError(f'Unknown state {state} with token {chr(c)}')
+    raise ValueError(f'Unexpected end at {state}')
+
+
+
+START = 1
+WANT_COLON = 2
+WANT_ATOMS = 3
--- tests/test_bencode.py
+++ tests/test_bencode.py
@@ -7,3 +7,7 @@ class TestBencode(TestCase):
     def test_encode(self):
         self.assertEqual(b'9:something', bencode.encode('something'))
         self.assertEqual(b'14:something', bencode.encode('something else'))
+
+    def test_decode(self):
+        self.assertEqual('something', bencode.decode(b'9:something'))
+        self.assertEqual('batata frita', bencode.decode(b'12:batata frita'))
